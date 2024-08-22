6a5affb7e06d80e15d3b520cdf3eaff3c51ed6f8
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-08-22 21:29:11 +0100

    chore: security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(cors());
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
